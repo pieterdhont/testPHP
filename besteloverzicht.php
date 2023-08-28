4cb66c3010382f631f278f50839a495784cc05a0
Pjotterismus <dhontpieter22@gmail.com>
2023-08-28 14:42:29 +0200

    .

--- besteloverzicht.php
+++ besteloverzicht.php
@@ -12,68 +12,55 @@ $bestellingen = $bestellingLijst->getBestellingen();
 <html>
 
 <head>
-  <title>Besteloverzicht</title>
-  <style>
-    .gemaakt {
-      background-color: #ddd;
-    }
-  </style>
+    <title>Besteloverzicht</title>
+    <style>
+        .gemaakt {
+            background-color: #ddd;
+        }
+    </style>
 </head>
 
 <body>
-  <h1>Besteloverzicht</h1>
-  <table>
-    <thead>
-      <tr>
-        <th>Voornaam</th>
-        <th>Achternaam</th>
-        <th>Broodje</th>
-        <th>Afhalingsmoment</th>
-        <th>Status</th>
-        <th>Acties</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach ($bestellingen as $bestelling): ?>
-        <tr class="<?= $bestelling->getStatus()->getStatus() === "Gemaakt" ? "gemaakt" : "" ?>">
-          <td>
-            <?= $bestelling->getKlant()->getVoornaam() ?>
-          </td>
-          <td>
-            <?= $bestelling->getKlant()->getAchternaam() ?>
-          </td>
-          <td>
-            <?= $bestelling->getBroodje()->getNaam() ?>
-          </td>
-          <td>
-            <?= $bestelling->getAfhalingsmoment() ?>
-          </td>
-          <td>
-            <?= $bestelling->getStatus()->getStatus() ?>
-          </td> <!-- Corrected line here -->
-          <td>
-            <?php if ($bestelling->getStatus()->getStatus() !== "Gemaakt"): ?> 
-              <form action="markAsGemaakt.php" method="post">
-                <input type="hidden" name="bestelID" value="<?= $bestelling->getBestelID() ?>">
+    <h1>Besteloverzicht</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Voornaam</th>
+                <th>Achternaam</th>
+                <th>Broodje</th>
+                <th>Afhalingsmoment</th>
+                <th>Status</th>
+                <th>Acties</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($bestellingen as $bestelling): 
                 
-                <input type="submit" value="Mark as Gemaakt">
-              </form>
-            <?php else: ?>
-              <form action="markAsAfgehaald.php" method="post">
-                <input type="hidden" name="bestelID" value="<?= $bestelling->getBestelID() ?>">
-                
-                <input type="submit" value="Mark as Afgehaald">
-              </form>
-            <?php endif; ?>
-          </td>
-        </tr>
-
-      <?php endforeach; ?>
-    </tbody>
-  </table>
+                // Preprocess data
+                $isGemaakt = $bestelling->getStatus()->getStatus() === "Gemaakt";
+                $action = $isGemaakt ? "markAsAfgehaald.php" : "markAsGemaakt.php";
+                $buttonLabel = $isGemaakt ? "Mark as Afgehaald" : "Mark as Gemaakt";
+            ?>
+            <tr class="<?= $isGemaakt ? "gemaakt" : "" ?>">
+                <td><?= $bestelling->getKlant()->getVoornaam() ?></td>
+                <td><?= $bestelling->getKlant()->getAchternaam() ?></td>
+                <td><?= $bestelling->getBroodje()->getNaam() ?></td>
+                <td><?= $bestelling->getAfhalingsmoment() ?></td>
+                <td><?= $bestelling->getStatus()->getStatus() ?></td>
+                <td>
+                    <form action="<?= $action ?>" method="post">
+                        <input type="hidden" name="bestelID" value="<?= $bestelling->getBestelID() ?>">
+                        <input type="submit" value="<?= $buttonLabel ?>">
+                    </form>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 
     <p><a href="index.html">Terug naar de Hoofdpagina</a></p>
 </body>
 
 </html>
+
 
